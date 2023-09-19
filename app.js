8dde6de2e2aac5fa1131e5c58d693a5796566168
Celeste Kilgore <celestekilgore@gmail.com>
2023-09-19 19:12:50 -0400

    added html form, started AJAX work

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+"use strict";
 console.log("Let's get this party started!");
+
+$("#form-submit").on("submit",searchGiphy);
+
+const apiKey = "aF0TcnfiZZLOXxKAJP5vJvXzuLW5iiFd";
+
+async function searchGiphy() {
+  const search = $("#search-term").val();
+  const params = new URLSearchParams({apiKey});
+  const response = await fetch("api.giphy.com/v1/gifs/search");
+  const data = await response.
+}
