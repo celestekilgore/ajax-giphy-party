efb2a95e5f958f2685b29c8241e3d10dd24ee7f5
Caitlyn Richmond <caitlyn.a.richmond@gmail.com>
2023-09-19 19:36:36 -0400

    Completed part two

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 "use strict";
 console.log("Let's get this party started!");
 
-$("#form-submit").on("submit",searchGiphy);
+$("#form-submit").on("click",searchGiphy);
 
-const apiKey = "aF0TcnfiZZLOXxKAJP5vJvXzuLW5iiFd";
 
-async function searchGiphy() {
+/**Searches giphy based on input and console logs resulting json */
+async function searchGiphy(evt) {
+  evt.preventDefault();
+  const apiKey = "aF0TcnfiZZLOXxKAJP5vJvXzuLW5iiFd";
   const search = $("#search-term").val();
-  const params = new URLSearchParams({apiKey});
-  const response = await fetch("api.giphy.com/v1/gifs/search");
-  const data = await response.
+  //http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym.
+  const params = new URLSearchParams({q: search, api_key: apiKey});
+  const response = await fetch(`https://api.giphy.com/v1/gifs/search?${params}`,{
+    headers: {
+      "Content-Type": "application/json"},
+  });
+  const data = await response.json();
+
+  const link = data.data[0].embed_url;
+  //TODO: random 0 for data.data.length
+
+  console.log(link);
+
+  const gif = $(`<iframe src=${link}></iframe>`);
+
+  const container = $("#container");
+  container.append(gif);
+
 }
