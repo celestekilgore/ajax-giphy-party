4e19e13892b80abc057821a92d7be0b8a6056e50
Celeste Kilgore <celestekilgore@gmail.com>
2023-09-19 20:06:02 -0400

    finished part 3

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 "use strict";
+
 console.log("Let's get this party started!");
 
-$("#form-submit").on("click",searchGiphy);
+$("#form-submit").on("submit", appendGif);
 
+$("#delete-button").on("click",deleteGifs);
 
-/**Searches giphy based on input and console logs resulting json */
-async function searchGiphy(evt) {
+/** Prevents default, calls searchGiphy, appends the image returned to DOM */
+async function appendGif(evt) {
   evt.preventDefault();
+  const link = await searchGiphy();
+  const $gif = $(`<iframe src=${link} border=${0}></iframe>`);
+  const $container = $("#container");
+  $container.append($gif);
+}
+
+/** Searches giphy based on input and returns link to a random GIF */
+async function searchGiphy() {
   const apiKey = "aF0TcnfiZZLOXxKAJP5vJvXzuLW5iiFd";
   const search = $("#search-term").val();
   //http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym.
-  const params = new URLSearchParams({q: search, api_key: apiKey});
-  const response = await fetch(`https://api.giphy.com/v1/gifs/search?${params}`,{
+  const params = new URLSearchParams({ tag: search, api_key: apiKey });
+  const response = await fetch(`https://api.giphy.com/v1/gifs/random?${params}`,{
     headers: {
-      "Content-Type": "application/json"},
+      "Content-Type": "application/json"
+    },
   });
+
   const data = await response.json();
 
-  const link = data.data[0].embed_url;
-  //TODO: random 0 for data.data.length
+  return data.data.embed_url;
+}
 
-  console.log(link);
-
-  const gif = $(`<iframe src=${link}></iframe>`);
-
-  const container = $("#container");
-  container.append(gif);
-
+/** Empties container of GIFs */
+function deleteGifs() {
+  const $container = $("#container");
+  $container.empty();
 }
